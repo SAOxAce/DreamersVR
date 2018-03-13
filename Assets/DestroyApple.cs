cbc4482b6933b28785441677bd75acaf477f7d7d
Denisse Ventura <d.ventura@ufl.edu>
2018-03-13 17:40:44 -0400

    Destroy Apple Script

---
+++ Assets/DestroyApple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyApple : MonoBehaviour {
+
+    public GameObject apple;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DestroyObject()
+    {
+        Destroy(apple);
+    }
+}
