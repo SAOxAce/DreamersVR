d16174890d8d3d8708cba1a8de4981501c8995ca
GregHarmon1 <greg.harmon35@gmail.com>
2018-03-15 13:17:42 -0400

    main menu
    hopefully this works

---
+++ Assets/Spring & Fall/Scene/Falling3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling3 : MonoBehaviour {
+
+
+
+	// Use this for initialization
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+		Invoke ("greg", 4);
+	}
+	void greg() {
+		transform.Translate (0, -.025f, 0);
+
+
+	}
+}
